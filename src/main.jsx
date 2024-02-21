03317c3c797731c0c2e882e9600bf9bcd5a13e06
Thanushan <thanushan.pirabakaran@ens.uvsq.fr>
2024-02-21 14:53:07 +0100

    Modification de la structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './Library.scss'
-import { ReactLenis } from '@studio-freight/react-lenis'
-import { BrowserRouter } from 'react-router-dom'
-
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReactLenis root>
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
-  </ReactLenis>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
