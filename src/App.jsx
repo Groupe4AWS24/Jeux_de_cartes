b205f152b509705912b9c24e08b91f0abfa316e3
Thanushan <thanushan.pirabakaran@ens.uvsq.fr>
2024-02-21 16:59:16 +0100

    Ajout du fond et gestion d'une carte

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,21 +14,30 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Component text="je sais pas"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="card-container">
+        <div className="card" id="card1"></div>
+        <div className="card" id="card2"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
+const Component = (props) => {
+  return(
+    <p>
+      {props.text}
+    </p>
+  );
+}
+
 export default App
+
+/* J'ai plus envie de coder, donc j'écris les idées, alors la gestions des cartes des utilisateur stocké 
+dans une liste, s'il possède moins de 7 cartes dans la liste le dernier element sera null, si 6 les deux
+derniers seront null, afficher la premiere cartes au centre, puis une carte sur deux à droite puis à gauche
+de la principale
+pour la création des cartes (affichage) passer par des props
+pareil pour la couleur actuel passer par des props
+*/
