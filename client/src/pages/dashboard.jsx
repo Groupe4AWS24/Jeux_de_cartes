da0f8f2018e60f3ddf7c418bc8b9806657d46beb
Maya SANTINI <maya.santini@ens.uvsq.fr>
2024-04-27 13:13:20 +0200

    debut front dashboard

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,13 +1,20 @@
 // A remplacer avec un menu
 // Imports
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/authentication.css";
+import "../styles/dashboard.css";
 import axios from "axios";
 
 // Fonction Dashboard qui sera modifiée pour devenir un menu
 function Dashboard() {
   const navigate = useNavigate();
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked); // Inverse l'état de la case à cocher lorsqu'elle est cliquée
+  };
+
+  function CheckBox() {}
 
   /**
    *  Fonction asynchrone qui envoie une requête pour la déconnexion.
@@ -24,13 +31,54 @@ function Dashboard() {
 
   // Rendu
   return (
-    <div class="container" style={style}>
-      <h1 className="text-center">Welcome to the Dashboard</h1>
-      <p class="text-center">
-        <a href="#" class="logout" onClick={disconnect}>
-          Log Out
-        </a>
-      </p>
+    <div className="screendash">
+      <div className="LeftPart Sidedash">
+      <div className="containerdash">
+        <div className="CreateRoom">
+          <h2 className="text-center">Create Room</h2>
+          <div className="choice-group">
+            <label htmlFor="players" className="label">
+              Number of players
+            </label>
+            <button className="button2">2</button>
+            <button className="button3">3</button>
+            <button className="button4">4</button>
+          </div>
+          <div className="bot-group">
+            <label htmlFor="bots" className="label">
+              Fill with bots
+            </label>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+              className="checkbox"
+            />
+          </div>
+          <button className="button" type="submit">
+            Create Room
+          </button>
+        </div>
+        </div>
+      </div>
+      <div className="RightPart">
+      <div className="containerdash">
+        <div className="input-group">
+          <label htmlFor="room" className="label">
+            Room ID
+          </label>
+          <input
+            type="text"
+            id="room"
+            className="input"
+            placeholder="Enter your Room ID"
+          />
+        </div>
+        <button className="button" type="submit">
+          Join Room
+        </button>
+      </div>
+    </div>
     </div>
   );
 }
