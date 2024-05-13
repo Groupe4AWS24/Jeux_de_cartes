caf16aec9b5eaad8f50d624e3a8ced66e1c067e5
uvsq21919161 <thanushan.pirabakaran@ens.uvsq.fr>
2024-05-13 03:01:42 +0200

    ajout des origins pour le cors

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -16,7 +16,7 @@ const {
 router.use(
   cors({
     credentials: true,
-    origin: process.env.SERVER_CLIENT,
+    origin: [process.env.SERVER_CLIENT, "https://onegame-sepia.vercel.app/"],
   })
 );
 
